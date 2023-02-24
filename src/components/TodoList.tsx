fec00ef7575d1c36006d12728435b07bb4e58ab4
Sonatai <gamedevsonatai@yahoo.com>
2023-02-24 20:15:26 +0100

    add scss #1

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,8 +1,24 @@
 import { defineComponent } from 'vue';
+import './style.scss';
 
 export default defineComponent({
     name: 'TodoList',
     setup() {
-        return () => <>TEST 2</>;
+        return () => (
+            <div class="wrapper">
+                <div class="todo">
+                    <h1>List1</h1>
+                    <ul>
+                        <li>what</li>
+                        <li>what</li>
+                        <li>what</li>
+                        <li>what</li>
+                    </ul>
+                </div>
+                <div class="todo">
+                    <h1>List2</h1>
+                </div>
+            </div>
+        );
     },
 });
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
 import App from './App';
-import './style.css';
+import './style.scss';
 
 createApp(App).mount('#app');
